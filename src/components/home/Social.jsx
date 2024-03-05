6a8639a2fa50c96010fec10632940cf7cfb659c1
Rishita Srivastava <rixsri.13@gmail.com>
2024-03-05 19:10:56 +0530

    tooltip

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a href='https://leetcode.com/rishita1304' className='home__social-icon' target='_blank'>
-            <i className='uil uil-brackets-curly'></i>
+        <a href='https://leetcode.com/rishita1304' className='home__social-icon tooltip' target='_blank'>
+            <i className='uil uil-brackets-curly'><span class="tooltiptext">Leetcode</span></i>
         </a>
-        <a href='https://www.linkedin.com/in/rishita1304/' className='home__social-icon' target='_blank'>
-        <i className='uil uil-linkedin'></i>
+        <a href='https://www.linkedin.com/in/rishita1304/' className='home__social-icon tooltip' target='_blank'>
+        <i className='uil uil-linkedin'><span class="tooltiptext">Linkedin</span></i>
         </a>
-        <a href='https://www.github.com/rishita1304/' className='home__social-icon' target='_blank'>
-        <i className='uil uil-github-alt'></i>
+        <a href='https://www.github.com/rishita1304/' className='home__social-icon tooltip' target='_blank'>
+        <i className='uil uil-github-alt'><span class="tooltiptext">Github</span></i>
         </a>
     </div>
   )
